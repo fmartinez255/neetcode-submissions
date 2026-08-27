71bc04dfe1b8e2fbe2390ffba35ac1ac91fa8311
Francisco Martinez <18524627+fmartinez255@users.noreply.github.com>
2026-08-27 16:24:35 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False # can't be true if uneven number of brackets
+
+        pStack = []
+
+        for bracket in list(s):
+            if bracket == "[" or bracket == "{" or bracket == "(":
+                pStack.append(bracket)
+
+            else:
+                if len(pStack) == 0:
+                    return False
+                br = pStack.pop()
+                if br == "{" and bracket != "}":
+                    return False
+                if br == "(" and bracket != ")":
+                    return False
+                if br == "[" and bracket != "]":
+                    return False
+
+        return True if len(pStack) == 0 else False
