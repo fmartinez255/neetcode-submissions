2bac9eabb77458cc90eb933288663c39df11357c
Francisco Martinez <18524627+fmartinez255@users.noreply.github.com>
2026-09-05 10:47:54 -0700

    Add: design-linked-list - submission-13

---
+++ Data Structures & Algorithms/design-linked-list/submission-13.py
@@ -0,0 +1,58 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MyLinkedList:
+    def __init__(self):
+        self.dummy = ListNode(0)
+        self.length = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.length:
+            return -1
+
+        curr = self.dummy
+
+        for _ in range(index+1):
+            curr = curr.next
+
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val: int) -> None:
+        self.addAtIndex(self.length, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index > self.length or index < 0:
+            return
+
+        curr = self.dummy
+
+        for _ in range(index):
+            curr = curr.next
+
+        curr.next = ListNode(val, curr.next)
+        self.length += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.length:
+            return
+
+        curr = self.dummy
+
+        for _ in range(index):
+            curr = curr.next
+
+        curr.next = curr.next.next
+        self.length -= 1
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
